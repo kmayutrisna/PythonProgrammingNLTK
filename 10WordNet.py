a3c7e475a9915e0c0357b1f53320f17968437dc3
kmayutrisna <45419531+kmayutrisna@users.noreply.github.com>
2019-09-25 12:57:17 +0800

    Create 10WordNet.py

---
+++ 10WordNet.py
@@ -0,0 +1,15 @@
+from nltk.corpus import wordnet
+#syns = wordnet.synsets("program")
+#print(syns[0].name())
+
+synonyms = []
+antonyms = []
+
+for syn in wordnet.synsets("good"):
+    for l in syn.lemmas():
+        synonyms.append(l.name())
+        if l.antonyms():
+            antonyms.append(l.antonyms()[0].name())
+
+print(set(synonyms))
+print(set(antonyms))

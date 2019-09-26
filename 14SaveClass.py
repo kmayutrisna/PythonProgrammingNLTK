e9319738a15a1289f7d6de6043f1447eb385aaec
kmayutrisna <45419531+kmayutrisna@users.noreply.github.com>
2019-09-26 13:26:23 +0800

    Update 14SaveClass.py

--- 14SaveClass.py
+++ 14SaveClass.py
@@ -1,6 +1,7 @@
 import nltk
 import random
 from nltk.corpus import movie_reviews
+import pickle
 
 documents = [(list(movie_reviews.words(fileid)), category)
                 for category in movie_reviews.categories()
@@ -9,27 +10,27 @@ documents = [(list(movie_reviews.words(fileid)), category)
 random.shuffle(documents)
 
 all_words = []
+
 for w in movie_reviews.words():
     all_words.append(w.lower())
 
 all_words = nltk.FreqDist(all_words)
 
-word_features = list(all_words.keys())[:3000]  #only the top 3000 most common words
+word_features = list(all_words.keys())[:3000]
 
-def find_features(documents):
-    words = set(documents)
+def find_features(document):
+    words = set(document)
     features = {}
     for w in word_features:
         features[w] = (w in words)
-    
-    return features
 
-#print((find_features(movie_reviews.words('neg/cv000_29416.txt'))))
+    return features
 
 featuresets = [(find_features(rev), category) for (rev, category) in documents]
 
 training_set = featuresets[:1900]
-testing_set =  featuresets[1900:]
+testing_set = featuresets[1900:]
+
 
 #classifier = nltk.NaiveBayesClassifier.train(training_set)
 
@@ -39,3 +40,7 @@ classifier_f.close()
 
 print("Naive Bayes Algo accuracy percent:", (nltk.classify.accuracy(classifier, testing_set))*100)
 classifier.show_most_informative_features(15)
+
+# save_classifier = open("naivebayes.pickle","wb")
+# pickle.dump(classifier, save_classifier)
+# save_classifier.close()
